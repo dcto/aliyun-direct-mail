30c206170678a50473674ad149c6bdb2e651eb21
陶之11 <sdoz@live.com>
2021-03-03 13:42:50 +0800

    update new version to direct-mail

--- src/Mail.php
+++ src/Mail.php
@@ -4,82 +4,190 @@ namespace Varimax\Aliyun;
 
 require_once('../lib/aliyun-php-sdk-core/Config.php');
 
-use Dm\Request\V20151123 as Dm; 
+use ClientException;
+use Dm\Request\V20151123 as Dm;
+use ServerException;
 
 class Mail{
-    protected $regionId;
-    protected $accessKeyId;
-    protected $accessSecret;
+    /**
+     * 配置选项
+     * @var array
+     */
+    protected $config = [
+        'accountName'=>'',//管理控制台中配置的发信地址
+        'regionId'=>'cn-hangzhou',
+        'accessKeyId'=>'',
+        'accessSecret'=>'',
+        'addressType'=>0,//地址类型 0：为随机账号 1：为发信地址
+        'replyToAddress'=>'',//使用管理控制台中配置的回信地址（状态必须是验证通过）。
+        'clickTrace'=>0,//0（默认）：为关闭数据跟踪功能。 1：为打开数据跟踪功能
+    ];
 
-    public function __construct($regionId, $accessKeyId, $accessSecret)
+    /**
+     * 发信人昵称，长度小于15个字符。
+     * 例如:发信人昵称设置为”小红”，发信地址为 test@example.com，收信人看到的发信地址为"小红"<test@example.com>。
+     * @var mixed
+     */
+    protected $from;
+
+    /**
+     * 邮件标签
+     * @var mixed
+     */
+    protected $tags;
+
+    /**
+     * 邮件 html 正文，限制28K
+     * @var mixed
+     */
+    protected $body;
+
+    /**
+     * 邮件列表名称
+     * 预先创建且上传了收件人的收件人列表名称。
+     * @var mixed
+     */
+    protected $list;
+
+    /**
+     * 邮件模板名称
+     * 控制台预先创建且通过审核的模板名称。
+     * @var mixed
+     */
+    protected $template;
+
+    /**
+     * 邮件主题，建议填写
+     */
+    protected $subject;
+
+
+
+    /**
+     * 配置邮件
+     * @param mixed $key 
+     * @param mixed|null $value 
+     * @return $this 
+     */
+    public function config($key = null, $value = null)
     {
-        $this->regionId = $regionId;
-        $this->accessKeyId = $accessKeyId;
-        $this->accessSecret = $accessSecret;
+        if(!$key) {
+            return $this->config;
+
+        }else if(is_array($key)){
+            $this->config = array_merge($this->config, $key);
+            
+        }else if($value){
+            $this->config[$key] = $value;
+        }else{
+            return isset($this->config[$key]) ? $this->config[$key] : null;
+        }
+        return $this;
+    }
+
+
+    public function to(...$mail)
+    {
+        return $this->mail($mail);
     }
 
     /**
-     * 发送单条邮件
-     * @param string $accountName 管理控制台中配置的发信地址
-     * @param string $fromAlias 发信人昵称，长度小于15个字符。
-     * 例如:发信人昵称设置为”小红”，发信地址为 test@example.com，收信人看到的发信地址为"小红"<test@example.com>。
-     * @param string $toAddress 目标地址，多个 email 地址可以用逗号分隔，最多100个地址。
-     * @param string $subject 邮件主题，建议填写
-     * @param string $htmlBody 邮件 html 正文，限制28K
-     * @param int $addressType 地址类型 0：为随机账号 1：为发信地址
-     * @param bool $replyToAddress 使用管理控制台中配置的回信地址（状态必须是验证通过）。
-     * @param int $clickTrace 0（默认）：为关闭数据跟踪功能。 1：为打开数据跟踪功能
-     * @return mixed|\SimpleXMLElement
+     * set email address
+     * @param mixed $email 
+     * @return $this 
      */
-    public function singleSend(
-        $accountName,
-        $fromAlias,
-        $toAddress,
-        $subject,
-        $htmlBody,
-        $tagName,
-        $addressType = 1,
-        $replyToAddress = false,
-        $clickTrace = 1)
+    public function mail(...$mail)
     {
-        $profile = \DefaultProfile::getProfile($this->regionId, $this->accessKeyId, $this->accessSecret);
-        $client = new \DefaultAcsClient($profile);
+        $this->mail = $mail;
+        return $this;
+    }
+
+
+    /**
+     * 邮件标签s
+     * @param mixed $tags 
+     * @return $this 
+     */
+    public function tags($tags)
+    {
+        $this->tags = $tags;
+        return $this;
+    }
+
+    /**
+     * mail body
+     * 
+     * @param mixed $content 
+     * @return $this 
+     */
+    public function body($content)
+    {
+        $this->body = $content;
+        return $this;
+    }
+
+
+    /**
+     * 地址发送邮件
+     * @return void 
+     */
+    public function send(...$email)
+    {
+        return $this->singleSend(join(',', $email));
+    }
+
+    /**
+     * 发送邮件列表
+     * @param mixed $list 邮件列表名称
+     * @param mixed $template 模板名称
+     * @return void 
+     */
+    public function list($list, $template)
+    {
+        return $this->batchSend($list, $template);
+    }
+
+    /**
+     * 单邮件发送
+     * @return mixed 
+     * @throws ClientException 
+     * @throws ServerException 
+     */
+    protected function singleSend($toAddress)
+    {
+        $client = new \DefaultAcsClient(\DefaultProfile::getProfile($this->config('regionId'), $this->config('accessKeyId'), $this->config('accessSecret')));
         $request = new Dm\SingleSendMailRequest();
-        $request->setAccountName($accountName);
-        $request->setFromAlias($fromAlias);
-        $request->setAddressType($addressType);
-        $request->setClickTrace($clickTrace);
-        $request->setTagName($tagName);
-        $request->setReplyToAddress($replyToAddress);
+        $request->setAccountName($this->config('accountName'));
+        $request->setReplyToAddress($this->config('replyToAddress'));
+        $request->setAddressType($this->config('addressType'));
+
+        $request->setFromAlias($this->from);
         $request->setToAddress($toAddress);
-        $request->setSubject($subject);
-        $request->setHtmlBody($htmlBody);
+        $request->setClickTrace($this->clickTrace);
+        $request->setTagName($this->tags);
+        $request->setHtmlBody($this->body);
+        $request->setSubject($this->subject);
+
         return $client->getAcsResponse($request);
     }
 
     /**
-     * 批量发送邮件
-     * @param string $accountName 管理控制台中配置的发信地址。
-     * @param string $templateName 预先创建且通过审核的模板名称。
-     * @param string $receiversName 预先创建且上传了收件人的收件人列表名称。
-     * @param string $tagName 邮件标签名称。
-     * @param int $addressType 地址类型 0：为随机账号 1：为发信地址
-     * @param int $clickTrace 0（默认）：为关闭数据跟踪功能。 1：为打开数据跟踪功能
-     * @return mixed|\SimpleXMLElement
+     * 批量邮件发磅
+     * @return mixed 
+     * @throws ClientException 
+     * @throws ServerException 
      */
-    public function batchSend($accountName, $templateName, $receiversName, $tagName, $addressType = 1, $clickTrace = 1)
+    protected function batchSend($list, $template)
     {
-        $profile = \DefaultProfile::getProfile($this->regionId, $this->accessKeyId, $this->accessSecret);
-        $client = new \DefaultAcsClient($profile);
+        $client = new \DefaultAcsClient(\DefaultProfile::getProfile($this->config('regionId'), $this->config('accessKeyId'), $this->config('accessSecret')));
         $request = new Dm\BatchSendMailRequest();
-        // 模板名称
-        $request->setTemplateName($templateName);
-        // 收件人列表名称
-        $request->setReceiversName($receiversName);
-        $request->setTagName($tagName);
-        $request->setAccountName($accountName);
-        $request->setAddressType($addressType);
-        $request->setClickTrace($clickTrace);
+        $request->setTemplateName($template);
+        $request->setReceiversName($list);
+        $request->setTagName($this->tags);
+        $request->setAccountName($this->config('accountName'));
+        $request->setAddressType($this->config('addressType'));
+        $request->setClickTrace($this->config('clickTrace'));
+
         return $client->getAcsResponse($request);
     }
 }
